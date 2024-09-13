4cc7465e80154721da0f656e4dc4f460885ee980
Sergey Abraztsou <sergeyabraztsou@Sergeys-MacBook-Air.local>
2024-09-14 01:06:31 +0300

    add user_id

--- src/views/PetForm.jsx
+++ src/views/PetForm.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {useNavigate, useParams} from "react-router-dom";
 import axiosClient from "../axiosClient";
 
 export default function PetForm() {
-    const { id } = useParams();
+    const {id} = useParams();
     const navigate = useNavigate();
     const [pet, setPet] = useState({
         id: null,
@@ -11,31 +11,50 @@ export default function PetForm() {
         nickname: '',
         date_of_birth: '',
         about: '',
-        user_id: '9d007644-821d-4260-92d6-b06af4c04e2b',
-        type_id: '9d007644-8559-42c0-865b-66afcfc189ef',
-        sub_type_id: '9d007644-9486-49b7-8520-5668f701ae62',
+        user_id: '',
+        type_id: '',
+        sub_type_id: '',
     });
+
+    const [users, setUsers] = useState([]);
+    const [petTypes, setPetTypes] = useState([]);
+    const [petSubTypes, setPetSubTypes] = useState([]);
 
     const [loading, setLoading] = useState(false);
     const [errors, setErrors] = useState(null);
 
     if (id) {
         useEffect(() => {
-            setLoading(true)
-            axiosClient.get(`http://127.0.0.1:80/api/v1/pets/${id}`)
-                .then(({ data }) => {
-                    setLoading(false)
-                    setPet(data.data)
-                })
-                .catch(() => {
-                    setLoading(false)
-                })
-        }, [])
+            const fetchData = async () => {
+                try {
+                    setLoading(true)
+
+                    const petResponse = await axiosClient.get(`/pets/${id}`);
+                    setPet(petResponse.data.data);
+
+                    const usersResponse = await axiosClient.get('/users');
+                    setUsers(usersResponse.data.data);
+
+                    const petTypesResponse = await axiosClient.get('/pet_types');
+                    setPetTypes(petTypesResponse.data.data);
+
+                    const petSubTypesResponse = await axiosClient.get('/pet_sub_types');
+                    setPetSubTypes(petSubTypesResponse.data.data);
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                } finally {
+                    setLoading(false);
+                }
+            };
+
+            fetchData();
+        }, []);
     }
 
     const onSubmit = ev => {
         ev.preventDefault()
         if (pet.id) {
+            console.log(pet)
             axiosClient.put(`/pets/${pet.id}`, pet)
                 .then(() => {
                     navigate('/pets')
@@ -79,12 +98,56 @@ export default function PetForm() {
                 }
                 {!loading && (
                     <form onSubmit={onSubmit}>
-                        <input value={pet.identifier} onChange={ev => setPet({ ...pet, identifier: ev.target.value })}
+                        <input value={pet.identifier} onChange={ev => setPet({...pet, identifier: ev.target.value})}
                                placeholder="Identifier"/>
-                        <input value={pet.nickname} onChange={ev => setPet({ ...pet, nickname: ev.target.value })}
+                        <input value={pet.nickname} onChange={ev => setPet({...pet, nickname: ev.target.value})}
                                placeholder="Nickname"/>
-                        <input value={pet.about} onChange={ev => setPet({ ...pet, about: ev.target.value })}
+                        <input value={pet.about} onChange={ev => setPet({...pet, about: ev.target.value})}
                                placeholder="About"/>
+                        <div>
+                            <select
+                                id="user"
+                                value={pet.user_id}
+                                onChange={(e) => setPet({...pet, user_id: e.target.value})}
+                            >
+                                <option value="">Select user</option>
+                                {users.map((user) => (
+                                    <option key={user.id} value={user.id}>
+                                        {user.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+
+                        <div>
+                            <select
+                                id="pet-type"
+                                value={pet.type_id}
+                                onChange={(e) => setPet({...pet, type_id: e.target.value})}
+                            >
+                                <option value="">Select pet type</option>
+                                {petTypes.map((type) => (
+                                    <option key={type.id} value={type.id}>
+                                        {type.title}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+
+                        <div>
+                            <select
+                                id="pet-sub-type"
+                                value={pet.sub_type_id}
+                                onChange={(e) => setPet({...pet, sub_type_id: e.target.value})}
+                            >
+                                <option value="">Select pet sub type</option>
+                                {petSubTypes.map((subType) => (
+                                    <option key={subType.id} value={subType.id}>
+                                        {subType.title}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
                         <button className="btn">Save</button>
                     </form>
                 )}
